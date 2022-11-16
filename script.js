e127aace3b94bb4fd834add9ec37d15b10fea655
Grady253 <grady.andre2@gmail.com>
2022-11-15 16:17:56 -0800

    worked on html page and create delete search history button.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const weatherApiRoot= 'https://api.openweathermap.org';
+const weatherApiRoot ='https://api.openweathermap.org';
 const apiKey='0753a0c64930e35683f0206250563635';
+const searchHist = [];
 
+const searchBtn = document.querySelector('#citySearchButton');
 
+function deleteSearch(){
+    localStorage.clear();
+};
